4b17b3565ae2979cc37016c0ac05d5da51295cbf
DT Norris <dtnorris@pm.me>
2026-08-10 04:50:45 -0400

    add minitest-mock

--- test/converter_qpdf_extraction_test.rb
+++ test/converter_qpdf_extraction_test.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "minitest/autorun"
+require "minitest/mock"
 require "tmpdir"
 require "yaml"
 require_relative "../lib/converter"
--- test/docling_adapter_ocr_test.rb
+++ test/docling_adapter_ocr_test.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "minitest/autorun"
+require "minitest/mock"
 require "tmpdir"
 require_relative "../lib/docling_adapter"
 
